34e8326ddca8bab556a5fd55b2e451903497a0eb
yaj55billy <yaj55ushydna@gmail.com>
2024-02-19 17:22:28 +0800

    Updates

---
+++ assets/NotFound-phTPQigc.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-0uMLGOXz.js";const n={},c={class:"mt-5"},_=o("h1",null,"404 Not Found",-1),r=o("p",null,"Sorry, the page you are looking for does not exist.",-1),a=[_,r];function d(i,l){return t(),s("div",c,a)}const f=e(n,[["render",d]]);export{f as default};
